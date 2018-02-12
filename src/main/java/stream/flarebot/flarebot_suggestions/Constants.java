6c57db3fb3bdacb92c194cb8d3c712697ae5297a
William Oldham <wegg7250@gmail.com>
2018-02-12 20:24:26 +0000

    Noone saw this

--- src/main/java/stream/flarebot/flarebot_suggestions/Constants.java
+++ src/main/java/stream/flarebot/flarebot_suggestions/Constants.java
@@ -2,12 +2,8 @@ package stream.flarebot.flarebot_suggestions;
 
 public class Constants {
 
-//    public static final long GUILD = 226785954537406464L;
-//    public static final long SUGGESTIONS_CHANNEL = 372450997466824714L;
-//    public static final long STAFF_ROLE = 320327762881675264L;
-
-    public static final long GUILD = 255063996611166220L;
-    public static final long SUGGESTIONS_CHANNEL = 412673005169410077L;
-    public static final long STAFF_ROLE = 255428839746961408L;
+    public static final long GUILD = 226785954537406464L;
+    public static final long SUGGESTIONS_CHANNEL = 372450997466824714L;
+    public static final long STAFF_ROLE = 320327762881675264L;
 
 }
